49025581ffb4e15d7a1bdfa89343444f36eb810c
Kevin Maurer <kevinmaurer2011@gmail.com>
2021-11-06 20:54:06 -0500

    added tax rates, taxable labor.

--- main.py
+++ main.py
@@ -2,23 +2,46 @@ from openpyxl import load_workbook
 
 workbook = load_workbook(filename='rates.xlsx')
 
-
 zip_code = input('Zip Code?')
 # zip_code = '00501'
 zipint = int(zip_code)
-sheet = workbook['page1']
+labor = workbook['labor']
+tax = workbook['tax']
 ziprow = 0
+taxrow = 0
 
-for i in sheet['A']:
+for i in labor['A']:
     if i.value == zipint:
         ziprow = i.row
 
+for i in tax['B']:
+    if i.value == zipint:
+        taxrow = i.row
 
-print('Found in spreadsheet row: ' + str(ziprow))
-print('Body:  ' + str(sheet.cell(row=ziprow,column=2).value))
-print('Paint: ' + str(sheet.cell(row=ziprow,column=3).value))
-print('Mech:  ' + str(sheet.cell(row=ziprow,column=4).value))
-print('Frame: ' + str(sheet.cell(row=ziprow,column=5).value))
-print('P & M: ' + str(sheet.cell(row=ziprow,column=6).value))
+try:
+    combined_tax = tax.cell(row=taxrow, column=4).value
+    tax_percent = (float(combined_tax)) * 100
+except:
+    print('No tax found sorry')
 
+try:
+    print('Body:  ' + str(labor.cell(row=ziprow, column=2).value))
+    print('Paint: ' + str(labor.cell(row=ziprow, column=3).value))
+    print('Mech:  ' + str(labor.cell(row=ziprow, column=4).value))
+    print('Frame: ' + str(labor.cell(row=ziprow, column=5).value))
+    print('P & M: ' + str(labor.cell(row=ziprow, column=6).value))
+except:
+    print('No labor rates found')
 
+try:
+    print('Tax: ' + str(tax_percent))
+except:
+    pass
+
+labor_taxed_states = (
+'AR', 'CT', 'FL', 'HI', 'IA', 'KS', 'MD', 'MN', 'MS', 'NE', 'NJ', 'NY', 'OH', 'PA', 'SD', 'TX', 'WV', 'WI')
+
+if tax.cell(row=taxrow, column=1).value in labor_taxed_states:
+    print('Labor Taxed: YES!')
+else:
+    print('Labor Taxed: No')

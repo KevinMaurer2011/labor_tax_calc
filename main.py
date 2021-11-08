456b1f09642730b7e028695ce172fbbb097b479c
Kevin Maurer <kevinmaurer2011@gmail.com>
2021-11-08 14:31:14 -0600

    added tax rates, taxable labor.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from openpyxl import load_workbook
 workbook = load_workbook(filename='rates.xlsx')
 
 zip_code = input('Zip Code?')
-# zip_code = '00501'
 zipint = int(zip_code)
 labor = workbook['labor']
 tax = workbook['tax']
@@ -30,18 +29,23 @@ try:
     print('Mech:  ' + str(labor.cell(row=ziprow, column=4).value))
     print('Frame: ' + str(labor.cell(row=ziprow, column=5).value))
     print('P & M: ' + str(labor.cell(row=ziprow, column=6).value))
+
 except:
     print('No labor rates found')
 
 try:
     print('Tax: ' + str(tax_percent))
+
 except:
     pass
 
 labor_taxed_states = (
-'AR', 'CT', 'FL', 'HI', 'IA', 'KS', 'MD', 'MN', 'MS', 'NE', 'NJ', 'NY', 'OH', 'PA', 'SD', 'TX', 'WV', 'WI')
+    'AR', 'CT', 'FL', 'HI', 'IA', 'KS', 'MD', 'MN', 'MS', 'NE', 'NJ', 'NY', 'OH', 'PA', 'SD', 'TX', 'WV', 'WI')
 
 if tax.cell(row=taxrow, column=1).value in labor_taxed_states:
     print('Labor Taxed: YES!')
 else:
     print('Labor Taxed: No')
+    print('')
+    print('')
+    print('')
